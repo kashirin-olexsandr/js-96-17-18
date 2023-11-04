ab0d71921487e99960a2846b081039623dabd2ce
Dok6857 <141351808+Dok6857@users.noreply.github.com>
2023-11-04 11:21:18 +0200

    complete

---
+++ JS/lesson2.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Що виведе наступний код?
+// let fruits = ["Яблука", "Груша", "Апельсин"];
+// Додаємо нове значення в "копію"
+// let shoppingCart = fruits;
+// shoppingCart.push("Банан");
+// що у fruits?
+// console.lo( fruits.length );
+
+//Напишіть функцію min(a, b), яка повертає
+//менше з чисел a, b
+//Потрібно додати перевірку, що функція отримує числа
+
+// function min(a, b) {
+//   if (typeof a === "number" && typeof b === "number") {
+//     return a < b ? a : b;
+//   }
+// }
+
+// console.log(min(15, 10));
