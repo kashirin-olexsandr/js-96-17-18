507d14e0944b9f4dad02b90bf4dcc6549ff76235
nikita-kotliar <knr200710@gmail.com>
2023-11-04 11:40:14 +0200

    Done

--- JS/lesson2.js
+++ JS/lesson2.js
@@ -19,3 +19,20 @@
 // }
 
 // console.log(min(15, 10));
+
+//Напиши функцію findSmallerNumber(numbers)
+//яка шукає найменше число в масиві.
+//Додай перевірку що функція отримує масив
+// const numbers = [12, 5, 35, 56, 3, 24, 7, 80, 12];
+
+function findSmallerNumber(numbers) {
+    let min = numbers[0];
+    for (let number of numbers) {
+        if (number < min) {
+            min = number;
+        }
+    }
+    return min;
+}
+
+console.log(findSmallerNumber([12, 5, 35, 56, 3, 24, 7, 80, 12]))
